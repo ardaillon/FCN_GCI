e89c4a95df54b45b4df471d24bd579a47b9b563a
Luc Ardaillon <luc.ardaillon@gmail.com>
2019-11-06 16:49:34 +0100

    moved code for target prediction into a separate file than run_analysis

---
+++ predict_target.py
@@ -0,0 +1,117 @@
+
+import numpy as np
+import warnings
+
+
+def db2lin(vec) :
+    '''
+    Convert values from dB to linear
+    :param vec: input values on a linear scale
+    :return: output values on a dB scale
+    '''
+    return 10**(vec/20.)
+
+
+def normalize_snd_file(inSnd, level = -3):
+    '''
+    Normalize input sound to have its maximum absolute value at the given level in dB
+    :param inSnd: input sound
+    :param level: maximum absolute value in dB
+    :return: normalized sound
+    '''
+    maxValIn = np.max(np.abs(inSnd))
+    maxValOut = db2lin(level)
+    scaleFactor = maxValOut / maxValIn
+    normSnd = inSnd * scaleFactor
+    return normSnd
+
+
+def get_audio(sndFile, model_input_size = 993, model_sr=16000.):
+    '''
+    Load and pre-process (make mono, resample, normalize, and pad) audio from input sound file
+    :param sndFile: input sound file
+    :param model_input_size: minimum input size of the model (necessary for padding input sound to have correct duration in output)
+    :return: audio, sound encoding tag
+    '''
+
+    # read sound :
+    # from scipy.io import wavfile
+    # (sr, audio) = wavfile.read(sndFile)
+    from pysndfile import sndio
+    (audio, sr, enc) = sndio.read(sndFile)
+
+    if len(audio.shape) == 2:
+        audio = audio.mean(1)  # make mono
+    audio = audio.astype(np.float32)
+
+    sndDuration = len(audio)/sr
+    print("duration of sound is "+str(sndDuration))
+
+    if sr != model_sr: # resample audio if necessary
+        from resampy import resample
+        audio = resample(audio, sr, model_sr)
+
+    audio = normalize_snd_file(audio)
+
+    # pad so that frames are centered around their timestamps (i.e. first frame is zero centered).
+    audio = np.pad(audio, int(model_input_size//2), mode='constant', constant_values=0)
+
+    return audio
+
+
+def segment_sound(snd, segDuration = 60., frame_sizes = 993, totalNetworkPoolingFactor = 8, model_sr=16000.):
+    '''
+    Segment input sound file into smaller chunks to avoid memory issues during inference
+    :param snd: input sound
+    :param segDuration: maximum duration of segments (in s)
+    :param frame_sizes: minimal size used by the network for prediction (necessary to have a correct connection of predicted values when reassembling the predicted vectors in the end)
+    :param totalNetworkPoolingFactor: total pooling factor (downsampling) applied by the max pooling layers in the network (necessary to have a correct connection of predicted values when reassembling the predicted vectors in the end)
+    :return: sound segments
+    '''
+    maxSndSegLenSamp = int(segDuration * model_sr)
+    maxSndSegLenSamp -= maxSndSegLenSamp%totalNetworkPoolingFactor
+    maxSndSegLenSamp += frame_sizes
+
+    sndSegments = []
+    i = 0
+    while(i < len(snd) - frame_sizes):
+        startIdx = i
+        endIdx = i+maxSndSegLenSamp
+        sndSegments.append(snd[startIdx:endIdx])
+        i = endIdx - frame_sizes + totalNetworkPoolingFactor
+
+    return sndSegments
+
+
+def speech_to_target(inFile, model, model_input_size=993, maxSndSegLenSec=60., totalNetworkPoolingFactor=8, verbose=True, model_sr=16000.):
+
+    # read and pad the audio from file :
+    snd = get_audio(inFile, model_input_size, model_sr=model_sr)
+
+    sndSegments = segment_sound(snd, segDuration=maxSndSegLenSec, frame_sizes=model_input_size,
+                                totalNetworkPoolingFactor=totalNetworkPoolingFactor, model_sr=model_sr)  #  segment sound in chunks for analysis
+
+    activations = []
+
+    for iseg, sndSeg in enumerate(sndSegments):
+        audio = np.array([np.reshape(sndSeg, (len(sndSeg), 1, 1))])
+        activationsSeg = model.predict(audio, verbose=verbose)
+        activationsSeg = np.reshape(activationsSeg, (np.shape(activationsSeg)[1]))
+        activations = np.concatenate((activations, activationsSeg))
+
+    time_src = np.arange(len(activations)) * totalNetworkPoolingFactor
+    time_target = np.arange(len(snd) - model_input_size + 1)
+    interpType = 'cubicSpline'
+    if (interpType == 'linear'):
+        targetPred = np.interp(time_target, time_src, activations)
+    elif (interpType == 'cubicSpline'):
+        from scipy.interpolate import CubicSpline
+        if (np.isnan(activations).any()):
+            warnings.warn("activations contains nan values")
+            noNaN_ids = np.where(np.isnan(activations) == False)[0]
+            time_src = time_src[noNaN_ids]
+            activations = activations[noNaN_ids]
+        interpolator = CubicSpline(time_src, activations)
+        targetPred = interpolator(time_target)
+    return targetPred
+
--- run_analysis.py
+++ run_analysis.py
@@ -21,53 +21,12 @@ from models.core import load_model
 import re
 from target_to_GCI import get_gci_times
 from fileio.sdif import mrk
+from predict_target import speech_to_target
+
 
 # model is trained for a sampling rate of 16000Hz
 model_sr = 16000.
 
-def db2lin(vec) :
-    '''
-    Convert values from dB to linear
-    :param vec: input values on a linear scale
-    :return: output values on a dB scale
-    '''
-    return 10**(vec/20.)
-
-def normalize_snd_file(inSnd, level = -3):
-    '''
-    Normalize input sound to have its maximum absolute value at the given level in dB
-    :param inSnd: input sound
-    :param level: maximum absolute value in dB
-    :return: normalized sound
-    '''
-    maxValIn = np.max(np.abs(inSnd))
-    maxValOut = db2lin(level)
-    scaleFactor = maxValOut / maxValIn
-    normSnd = inSnd * scaleFactor
-    return normSnd
-
-def segment_sound(snd, segDuration = 60., frame_sizes = 993, totalNetworkPoolingFactor = 8):
-    '''
-    Segment input sound file into smaller chunks to avoid memory issues during inference
-    :param snd: input sound
-    :param segDuration: maximum duration of segments (in s)
-    :param frame_sizes: minimal size used by the network for prediction (necessary to have a correct connection of predicted values when reassembling the predicted vectors in the end)
-    :param totalNetworkPoolingFactor: total pooling factor (downsampling) applied by the max pooling layers in the network (necessary to have a correct connection of predicted values when reassembling the predicted vectors in the end)
-    :return: sound segments
-    '''
-    maxSndSegLenSamp = int(segDuration * model_sr)
-    maxSndSegLenSamp -= maxSndSegLenSamp%totalNetworkPoolingFactor
-    maxSndSegLenSamp += frame_sizes
-
-    sndSegments = []
-    i = 0
-    while(i < len(snd) - frame_sizes):
-        startIdx = i
-        endIdx = i+maxSndSegLenSamp
-        sndSegments.append(snd[startIdx:endIdx])
-        i = endIdx - frame_sizes + totalNetworkPoolingFactor
-
-    return sndSegments
 
 def run_prediction(filename, output = None, modelTag = 'FCN_synth_tri', verbose = True):
     '''
@@ -127,39 +86,6 @@ def run_prediction(filename, output = None, modelTag = 'FCN_synth_tri', verbose 
     return
 
 
-def get_audio(sndFile, model_input_size = 993):
-    '''
-    Load and pre-process (make mono, resample, normalize, and pad) audio from input sound file
-    :param sndFile: input sound file
-    :param model_input_size: minimum input size of the model (necessary for padding input sound to have correct duration in output)
-    :return: audio, sound encoding tag
-    '''
-
-    # read sound :
-    # from scipy.io import wavfile
-    # (sr, audio) = wavfile.read(sndFile)
-    from pysndfile import sndio
-    (audio, sr, enc) = sndio.read(sndFile)
-
-    if len(audio.shape) == 2:
-        audio = audio.mean(1)  # make mono
-    audio = audio.astype(np.float32)
-
-    sndDuration = len(audio)/sr
-    print("duration of sound is "+str(sndDuration))
-
-    if sr != model_sr: # resample audio if necessary
-        from resampy import resample
-        audio = resample(audio, sr, model_sr)
-
-    audio = normalize_snd_file(audio)
-
-    # pad so that frames are centered around their timestamps (i.e. first frame is zero centered).
-    audio = np.pad(audio, int(model_input_size//2), mode='constant', constant_values=0)
-
-    return (audio, enc)
-
-
 def get_output_path(file, suffix, output_dir):
     '''
     Return the path of an output file corresponding to a wav file
@@ -195,41 +121,14 @@ def run_prediction_on_file(inFile, output=None, model=None, mode = 'GF', maxSndS
     if(model==None):
         raise('FCN_GCI: model is None')
 
-    # read and pad the audio from file :
-    (snd, enc) = get_audio(inFile, model_input_size)
-
-    sndSegments = segment_sound(snd, segDuration=maxSndSegLenSec, frame_sizes=model_input_size, totalNetworkPoolingFactor=totalNetworkPoolingFactor) # segment sound in chunks for analysis
-
-    activations = []
-
-    for iseg, sndSeg in enumerate(sndSegments):
-
-        audio = np.array([np.reshape(sndSeg, (len(sndSeg),1,1))])
-        activationsSeg = model.predict(audio, verbose=verbose)
-        activationsSeg = np.reshape(activationsSeg, (np.shape(activationsSeg)[1]))
-        activations = np.concatenate((activations, activationsSeg))
-
-    time_src = np.arange(len(activations))*totalNetworkPoolingFactor
-    time_target = np.arange(len(snd)-model_input_size+1)
-    interpType = 'cubicSpline'
-    if(interpType == 'linear'):
-        targetPred = np.interp(time_target, time_src, activations)
-    elif(interpType=='cubicSpline'):
-        from scipy.interpolate import CubicSpline
-        if(np.isnan(activations).any()):
-            warnings.warn("activations contains nan values")
-            noNaN_ids = np.where(np.isnan(activations)==False)[0]
-            time_src = time_src[noNaN_ids]
-            activations = activations[noNaN_ids]
-        interpolator = CubicSpline(time_src, activations)
-        targetPred = interpolator(time_target)
+    targetPred = speech_to_target(inFile, model, model_input_size=model_input_size, maxSndSegLenSec=maxSndSegLenSec, totalNetworkPoolingFactor=totalNetworkPoolingFactor, verbose=verbose, model_sr=model_sr)
 
     targetFile = get_output_path(inFile, ".targetPred.wav", output)
     outputDir = os.path.dirname(targetFile)
     if(not os.path.isdir(outputDir)):
         os.makedirs(outputDir)
     print("saving target shape analysis to file "+targetFile)
-    sndio.write(targetFile, targetPred, model_sr, 'wav', enc)
+    sndio.write(targetFile, targetPred, model_sr, 'wav', 'pcm16')
 
     GCI_times = get_gci_times(targetPred, model_sr, mode=mode)
 
@@ -249,8 +148,8 @@ if __name__ == '__main__':
 
     global_args = parser.add_argument_group('global')
     parser.add_argument('-i', "--input", nargs='+', default='./examples/speech16k-norm', help='path to one ore more WAV file(s) to analyze OR can be a directory')
-    global_args.add_argument('-o', "--output", default='./examples/pred_target_triangle', help='output f0 analysis (either a directory or a file with sdif extension)')
-    global_args.add_argument('-m', "--modelTag", default='FCN_synth_tri', help='name model to be used for prediction (default is FCN_synth_tri)')
+    global_args.add_argument('-o', "--output", default=None, help='output f0 analysis (either a directory or a file with sdif extension)')
+    global_args.add_argument('-m', "--modelTag", default='FCN_synth_GF', help='name model to be used for prediction (default is FCN_synth_GF)')
 
     args = parser.parse_args()
 
